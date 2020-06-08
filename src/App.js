14b8e44258eedc64d3774c2d615b010edf6ae978
JeJe1217 <jeaha1217@gmail.com>
2020-06-08 15:18:36 +0900

    Props를 통해 컴포넌트에 값 전달하기.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
 import './styles.css';
+import Wrapper from '../Wrapper';
 
 export default function App() {
-  const name = 'react';
-
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1em',
-  };
-  //  comment
-
   return (
-    <>
-      {/* comment */}
-      <Hello />
-      <br
-      // comment
-      />
-      <div style={style}>{name}</div>
-      <div className="grey-box" />
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Hello() {
-  return <div>HELLO</div>;
+function Hello({ name, color }) {
+  console.log(name, color);
+  return (
+    <div
+      style={{
+        color,
+      }}
+    >
+      HELLO {name}
+    </div>
+  );
 }
+
+Hello.defaultProps = {
+  name: 'no name',
+  color: 'dimgrey',
+};
 
 export default Hello;
