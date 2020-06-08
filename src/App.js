eddb061005c66024e1ab8cf606c6d93e21206e55
JeJe1217 <jeaha1217@gmail.com>
2020-06-08 14:46:52 +0900

    first react component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './styles.css';
+import Hello from './Hello';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>HIHI react application is created now.</p>
+      <Hello />
+      <Hello />
     </div>
   );
 }
